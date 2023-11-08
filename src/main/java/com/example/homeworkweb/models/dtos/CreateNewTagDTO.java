f93deab51c44471a50e9426e257787653450c1bf
Tomas Emr <tommy.emr@gmail.com>
2023-11-08 08:11:55 +0100

    model for creating new tag

---
+++ src/main/java/com/example/homeworkweb/models/dtos/CreateNewTagDTO.java
@@ -0,0 +1,15 @@
+package com.example.homeworkweb.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateNewTagDTO {
+
+        private String name;
+}
